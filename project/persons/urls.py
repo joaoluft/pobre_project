517da77573f04ad97685343ed5bebfdcf3806f71
João Luft <joao.luft@vortigo.com.br>
2024-10-21 18:16:21 -0300

    feature: Admin route

--- project/persons/urls.py
+++ project/persons/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib import admin
 from .views import PersonCreateView, PersonUpdateView, PersonDeleteView, person_list
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', person_list, name='person-list'),
     path('create/', PersonCreateView.as_view(), name='person-create'),
     path('<int:pk>/update/', PersonUpdateView.as_view(), name='person-update'),
